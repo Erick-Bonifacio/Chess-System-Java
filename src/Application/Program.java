07016447de3104536cda5d8be7d30ba7397b5966
Erick de Alvarenga Bonifacio <erickalvarengabonifacio@hotmail.com>
2024-01-09 20:18:23 -0300

    movimentando peças

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,16 +1,31 @@
 package Application;
 
+import java.util.Scanner;
+
 import BoardGame.Board;
 import BoardGame.Position;
-import Chess.ChessMatch; 
+import Chess.ChessMatch;
+import Chess.ChessPiece;
+import Chess.ChessPosition; 
 
 public class Program {
 
     public static void main(String[] args){
 
         ChessMatch chessMatch = new ChessMatch();
+        Scanner sc = new Scanner(System.in);
+        
+        while(true){
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.println("Source:");
+            ChessPosition source = UI.readChessPosition(sc);
+            System.out.println();
+            System.out.println("Target:");
+            ChessPosition target = UI.readChessPosition(sc);
 
-        UI.printBoard(chessMatch.getPieces());
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
 
     }
 }
