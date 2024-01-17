0d96deebc9d5e7655f91ba9498650d0732d454ff
Erick de Alvarenga Bonifacio <erickalvarengabonifacio@hotmail.com>
2024-01-17 14:44:06 -0300

    imprimindo possiveis movimentos

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -25,6 +25,12 @@ public class Program {
                 System.out.println();
                 System.out.println("Source:");
                 ChessPosition source = UI.readChessPosition(sc);
+
+                boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+                UI.clearScreen();
+                System.out.println();
+                UI.printBoard(chessMatch.getPieces(), possibleMoves);
+                
                 System.out.println();
                 System.out.println("Target:");
                 ChessPosition target = UI.readChessPosition(sc);
