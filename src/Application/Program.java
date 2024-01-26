1f74d49cae81ed260ea178370529fd4e26b06e03
Erick de Alvarenga Bonifacio <erickalvarengabonifacio@hotmail.com>
2024-01-26 19:25:42 -0300

    Melhor tratamento para promoção

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -46,7 +46,12 @@ public class Program {
 
                 if(chessMatch.getPromoted() != null){
                     System.out.println("Enter piece for promotion (B/Q/R/Q): ");
-                    String type = sc.nextLine();
+                    String type = sc.nextLine().toUpperCase();
+
+                    while(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")){
+                        System.out.println("Inavalid Value: Enter piece for promotion (B/Q/R/Q): ");
+                        type = sc.nextLine().toUpperCase();
+                    }
                     chessMatch.replacePromotedPiece(type);
                 }
              }
